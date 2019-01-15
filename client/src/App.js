69afebf3e8a553781e3697ffc3701ddd32e478a8
Jon Grant <jongrantwritescode@gmail.com>
2019-01-14 20:46:26 -0800

    basic auth files with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+import "./App.css";
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route
+              exact
+              path="/login"
+              component={SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION}
+            />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
